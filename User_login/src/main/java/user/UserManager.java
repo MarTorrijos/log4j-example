a926b0fd41ad622c12363b0f1d5678cbaaea73b2
mar <martorrijos@protonmail.com>
2025-07-30 10:50:27 +0200

    log: add new logs, still a wip

--- User_login/src/main/java/user/UserManager.java
+++ User_login/src/main/java/user/UserManager.java
@@ -12,7 +12,6 @@ import java.util.Map;
 
 import logs.*;
 
-
 public class UserManager {
 
     private static final Logger logger = LogManager.getLogger(UserManager.class.getName());
@@ -23,8 +22,7 @@ public class UserManager {
 
         if(users.containsKey(user.getName())) {
             System.out.println(UserMessage.USER_ALREADY_REGISTERED);
-            logger.debug(LoggingMsg.REGISTER_FAIL + ": `{}`", user.getName());
-            logger.warn("Already");
+            logger.debug(LoggingMsg.REGISTER_FAIL + "{}", user.getName());
             return;
 
         }
@@ -32,8 +30,8 @@ public class UserManager {
         try {
             objMapper.writeValue(new File("src/main/resources/users.json"), users);
             System.out.println(UserMessage.REGISTER_SUCCESS);
+            logger.info(LoggingMsg.REGISTER_SUCCESS + ". Usuario registrado: {}", user.getName());
         } catch (IOException e) {
-            //TODO: LOGGER
             throw new RuntimeException(e);
         }
     }
@@ -41,13 +39,16 @@ public class UserManager {
     public void validateUser(String name, String pass){
         if(!loadUsers().containsKey(name)){
             System.out.println(UserMessage.INVALID_CREDENTIALS);
+            logger.debug(LoggingMsg.LOGIN_FAIL + ". Usuario `{}` no existe", name);
             return;
         }
         if(!loadUsers().get(name).getPassword().equalsIgnoreCase(pass)) {
             System.out.println(UserMessage.INVALID_CREDENTIALS);
+            logger.debug(LoggingMsg.LOGIN_FAIL + ". Contraseña no válida");
             return;
         }
         System.out.println(UserMessage.LOGIN_SUCCESS);
+        logger.info(LoggingMsg.LOGIN_SUCCESS);
     }
 
     private Map<String, User> loadUsers(){
@@ -56,11 +57,11 @@ public class UserManager {
         if(file.length() < 1)
             return new HashMap<>();
         try {
-           return mapper.readValue(file, new TypeReference<HashMap<String, User>>() {});
+            return mapper.readValue(file, new TypeReference<HashMap<String, User>>() {});
         } catch (IOException e) {
+            logger.error(LoggingMsg.IO_FAIL + " Archivo inexistente en la ruta proporcionada");
             throw new RuntimeException(e);
         }
     }
-
 
 }
